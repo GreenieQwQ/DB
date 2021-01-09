7020cb7d6501aaded718c7e74fddaf9fb55e9540
zhanghaoc <2272055687@qq.com>
2021-01-09 12:26:44 +0800

    add deletion and modification

--- src/JTableDemo.java
+++ src/JTableDemo.java
@@ -8,6 +8,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import javax.swing.*;
+import javax.swing.event.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
@@ -17,6 +18,9 @@ public class JTableDemo extends JFrame
     JTable table;
     JPanel con;
     String dbName = "Worker";
+    String historyID = "";
+    String historysex = "";
+    String historyname = "";
 
     public static void main(String[] agrs) {
         Connector.initialize();
@@ -52,7 +56,8 @@ public class JTableDemo extends JFrame
         JPanel p3=new JPanel();
         JPanel p4=new JPanel();
         JPanel p5=new JPanel();
-        p5.setLayout(new GridLayout(6,1,5,5));
+        JPanel pALL=new JPanel();
+        pALL.setLayout(new GridLayout(7,1,5,5));
         Box bV=Box.createVerticalBox();
         Box bH1=Box.createHorizontalBox();
         Box bH2=Box.createHorizontalBox();
@@ -70,7 +75,77 @@ public class JTableDemo extends JFrame
 
         JButton buttonFind = new JButton("查询");
         JButton buttonCount = new JButton("插入");
+        JButton buttonDelete = new JButton("删除");
+        JButton buttonModify = new JButton("修改");
+//        table.getSelectionModel().addMouseListener(new MouseAdapter() {
+//            public void mouseClicked (MouseEvent me) {
+//                int[] selectedRows=table.getSelectedRows();
+//                for (int row : selectedRows) {
+//                    System.out.println(row);
+//                }
+//            }
+//        });
+//        table.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
+//            public void valueChanged(ListSelectionEvent event) {
+//                // will excute twice, for click on and off
+//                idText.setText(table.getValueAt(table.getSelectedRow(), 0).toString());
+//                sexText.setText(table.getValueAt(table.getSelectedRow(), 1).toString());
+//                nameText.setText(table.getValueAt(table.getSelectedRow(), 2).toString());
+//            }
+//        });
+        table.addMouseListener(new MouseAdapter() {
+            public void mouseClicked (MouseEvent me) {
+                // will excute twice, for click on and off
+                historyID = table.getValueAt(table.getSelectedRow(), 0).toString();
+                historysex = table.getValueAt(table.getSelectedRow(), 1).toString();
+                historyname = table.getValueAt(table.getSelectedRow(), 2).toString();
+                idText.setText(historyID);
+                sexText.setText(historysex);
+                nameText.setText(historyname);
+            }
+        });
+        buttonModify.addMouseListener(new MouseAdapter() {
+            public void mouseClicked (MouseEvent me) {
+                String id = idText.getText();
+                String sex = sexText.getText();
+                String name = nameText.getText();
+                String[] stringArr= {id, sex, name};
+                // 在StringArr记得处理空值
+                int flag = 0;
+                String temp = "UPDATE Worker";
+                if (!id.equals("")) {
+                    //new JOptionPane().showMessageDialog(null, "用户名不能为空！");
+                    if (flag == 0)
+                        temp += " SET ";
+                    temp += "id=\""+ id + "\"";
+                    flag += 1;
+                }
+                if (!sex.equals("")) {
+                    if (flag == 0)
+                        temp += " SET ";
+                    if (flag > 0)
+                        temp += " , ";
+                    temp += "sex=\""+ sex+"\"";
+                    flag += 1;
+                }
+                if (!name.equals("")) {
+                    if (flag == 0)
+                        temp += " SET ";
+                    if (flag > 0)
+                        temp += " , ";
+                    temp += "name=\""+ name+"\"";
+                    flag += 1;
+                }
+                temp += " WHERE id=\""+historyID+"\" AND sex=\""+historysex+"\" AND name=\""+historyname+"\";";
+                try {
+                    System.out.println(temp);
+                    modifyInfo(temp);
+                }
+                catch (SQLException ex){
 
+                }
+            }
+        });
         buttonFind.addMouseListener(new MouseAdapter() {
             public void mouseClicked (MouseEvent me) {
                 String id = idText.getText();
@@ -131,6 +206,47 @@ public class JTableDemo extends JFrame
                 }
             }
         });
+        buttonDelete.addMouseListener(new MouseAdapter() {
+            public void mouseClicked (MouseEvent me) {
+                String id = idText.getText();
+                String sex = sexText.getText();
+                String name = nameText.getText();
+                String[] stringArr= {id, sex, name};
+                // 在StringArr记得处理空值
+                int flag = 0;
+                String temp = "DELETE FROM Worker ";
+                if (!id.equals("")) {
+                    //new JOptionPane().showMessageDialog(null, "用户名不能为空！");
+                    if (flag == 0)
+                        temp += " WHERE ";
+                    temp += "id=\""+ id + "\"";
+                    flag += 1;
+                }
+                if (!sex.equals("")) {
+                    if (flag == 0)
+                        temp += " WHERE ";
+                    if (flag > 0)
+                        temp += " AND ";
+                    temp += "sex=\""+ sex+"\"";
+                    flag += 1;
+                }
+                if (!name.equals("")) {
+                    if (flag == 0)
+                        temp += " WHERE ";
+                    if (flag > 0)
+                        temp += " AND ";
+                    temp += "name=\""+ name+"\"";
+                    flag += 1;
+                }
+                try {
+                    System.out.println(temp);
+                    deleteInfo(temp);
+                }
+                catch (SQLException ex){
+                    System.out.println("Deletion failed!\n");
+                }
+            }
+        });
 //        bH1.add(Box.createVerticalStrut(20));
 //        bH2.add(Box.createVerticalStrut(20));
 //        bH1.add(lbH1);
@@ -151,11 +267,14 @@ public class JTableDemo extends JFrame
 
         p4.add(buttonFind);
         p4.add(buttonCount);
+        p5.add(buttonDelete);
+        p5.add(buttonModify);
 
-        p5.add(p1);
-        p5.add(p2);
-        p5.add(p3);
-        p5.add(p4);
+        pALL.add(p1);
+        pALL.add(p2);
+        pALL.add(p3);
+        pALL.add(p4);
+        pALL.add(p5);
 //        bV.add(bH1);
 //        bV.add(bH2);
 //        bV.add(bH3);
@@ -168,12 +287,18 @@ public class JTableDemo extends JFrame
         con.setBorder(new EmptyBorder(5,5,5,5));
         con.setLayout(new BorderLayout(0,0));
         setContentPane(con);
-        con.add(p5, BorderLayout.WEST);
+        con.add(pALL, BorderLayout.WEST);
         con.add(lb, BorderLayout.NORTH);
 
         JScrollPane scrollPane=new JScrollPane();
         con.add(scrollPane, BorderLayout.EAST);
         scrollPane.setViewportView(table);
+        try {
+            findInfo("select * from worker");
+        } catch (SQLException ex){
+
+        }
+
         //frame.setVisible(true);
     }
     // findInfo希望能传递参数object
@@ -204,15 +329,21 @@ public class JTableDemo extends JFrame
         // 防注入攻击
         // 预编译
 
-        PreparedStatement stmt = Connector.conn.prepareStatement(sql);
 //        for (int i = 0; i < args.length; i++) {
 //            stmt.setString(i, args[i]);
 //        }
+        ResultSetMetaData rsmd;
+        ResultSet rs;
         // 执行
-
-        ResultSet rs = Connector.executeQuery(stmt);
-
-        ResultSetMetaData rsmd = rs.getMetaData();
+        try {
+            PreparedStatement stmt = Connector.conn.prepareStatement(sql);
+            rs = Connector.executeQuery(stmt);
+            rsmd = rs.getMetaData();
+        }
+        catch (SQLException ex){
+            return;
+        }
+        //System.out.println("here------");
         // error在executeQuery会被catch到，能返回大概是空表，此时不出错
 //        if(rs!=null) {
 //            System.out.println("cannot find result?");
@@ -247,20 +378,69 @@ public class JTableDemo extends JFrame
         }  // 若连接失败返回
         System.out.println("Inserting data...");
         PreparedStatement stmt = Connector.conn.prepareStatement(sql);
+
+        try {
+            Integer rs = Connector.executeUpdate(sql);
+            if (rs == 0) {
+                System.out.println("Insertion failed!");
+                return;
+            }
+            System.out.println("Insertion succeed!");
+            sql = "select * from " + dbName;
+            findInfo(sql);
+        }
+        catch (SQLException ex){
+//            return;
+        }
+
+
+    }
+    public void deleteInfo(String sql) throws SQLException{
+        if(!Connector.initialize()) {
+            System.out.println("连接不上数据库...");
+            new JOptionPane().showMessageDialog(null,
+                    "连接不上数据库...",
+                    "",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }  // 若连接失败返回
+        System.out.println("Deleting data...");
+        PreparedStatement stmt = Connector.conn.prepareStatement(sql);
         Integer rs = Connector.executeUpdate(sql);
 
         if (rs == 0) {
-            System.out.println("Insertion failed!");
+            System.out.println("Deletion failed!");
             return;
         }
-        System.out.println("Insertion succeed!");
+        System.out.println("Deletion succeed!");
         sql = "select * from " + dbName;
         findInfo(sql);
 
+//        int[] selectedRows=table.getSelectedRows();
+//        for (int row : selectedRows) {
+//            tableModel.removeRow(0);
+//        }
+    }
+    public void modifyInfo(String sql) throws SQLException{
+        if(!Connector.initialize()) {
+            System.out.println("连接不上数据库...");
+            new JOptionPane().showMessageDialog(null,
+                    "连接不上数据库...",
+                    "",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }  // 若连接失败返回
+        System.out.println("Deleting data...");
+        PreparedStatement stmt = Connector.conn.prepareStatement(sql);
+        Integer rs = Connector.executeUpdate(sql);
 
-        int[] selectedRows=table.getSelectedRows();
-        //tableModel.removeRow(0);
-
+        if (rs == 0) {
+            System.out.println("Modification failed!");
+            return;
+        }
+        System.out.println("Modification succeed!");
+        sql = "select * from " + dbName;
+        findInfo(sql);
     }
     private Object[] appendValue(Object[] obj, Object newObj) {
 
